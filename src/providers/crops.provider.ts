3370b0061a89266fd7c9549b511650a1b25487c4
Esteban Morales <red_bandit_11@hotmail.com>
2017-07-07 11:48:27 -0400

    minor changes

--- src/providers/crops.provider.ts
+++ src/providers/crops.provider.ts
@@ -32,7 +32,7 @@ export class CropProvider {
             const err = body.error || JSON.stringify(body);
             errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
         } else {
-            errMsg = error.message ? error.message : error;
+            errMsg = error;
         }
         console.error(errMsg);
         return Observable.throw(errMsg);
